41cd413a3dbd50c43309db62e8728c883b45591d
Wes Tarver <westarver56@gmail.com>
2022-06-30 17:19:14 -0500

    initial commit

---
+++ go.mod
@@ -0,0 +1,10 @@
+module helper
+
+go 1.18
+
+require (
+	github.com/rhysd/abspath v0.0.0-20200817132137-9532ba017882
+	gopkg.in/ini.v1 v1.66.6
+)
+
+require github.com/stretchr/testify v1.7.5 // indirect
---
+++ helper.go
@@ -0,0 +1,132 @@
+package helper
+
+import (
+	"fmt"
+	"io"
+	"os"
+	"path/filepath"
+	"strings"
+
+	apath "github.com/rhysd/abspath"
+)
+
+//─────────────┤ GetLogFile ├─────────────
+
+func GetLogFile(file string) *os.File {
+	if file == "std" {
+		return os.Stderr
+	}
+	f, _ := OpenFile(file)
+	if f == nil {
+		fmt.Println("Failure to open Log file " + file + ". Will default to Stderr.")
+		f = os.Stderr
+	}
+	return f
+}
+
+//─────────────┤ OpenFile ├─────────────
+
+func OpenFile(file string) (*os.File, error) {
+	path, err := GetPath(file)
+
+	if err == nil {
+		return os.OpenFile(path, os.O_WRONLY|os.O_CREATE, 0600)
+	}
+	return nil, err
+}
+
+//─────────────┤ OpenAppend ├─────────────
+
+func OpenAppend(file string) (*os.File, error) {
+	path, err := GetPath(file)
+
+	if err == nil {
+		return os.OpenFile(path, os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0600)
+	}
+	return nil, err
+}
+
+//─────────────┤ OpenTrunc ├─────────────
+
+func OpenTrunc(file string) (*os.File, error) {
+	path, err := GetPath(file)
+
+	if err == nil {
+		return os.OpenFile(path, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0600)
+	}
+	return nil, err
+}
+
+//─────────────┤ CopyFileStr ├─────────────
+
+func CopyFileStr(dst, src string) error {
+	d, err := OpenTrunc(dst)
+	if err != nil {
+		return err
+	}
+	s, err := OpenFile(src)
+	if err != nil {
+		return err
+	}
+	_, _ = io.Copy(d, s)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+//─────────────┤ GetPath  ├─────────────
+
+func GetPath(name string) (string, error) {
+	a, err := apath.ExpandFrom(name)
+	return a.String(), err
+}
+
+//─────────────┤ DirExists ├─────────────
+
+func DirExists(dirname string) bool {
+	info, err := os.Stat(dirname)
+	return err == nil && info.IsDir()
+}
+
+//─────────────┤ validatePath ├─────────────
+
+func validatePath(path string) (string, error) {
+	a, err := apath.ExpandFrom(path)
+	if err != nil {
+		return "", err
+	}
+	path = a.String()
+	d, f := filepath.Split(path)
+	err = os.MkdirAll(d, 0777)
+	if err != nil {
+		return path, err
+	}
+	return d + f, nil
+}
+
+//─────────────┤ CountLinesInString  ├─────────────
+
+func CountLinesInString(s string) int {
+	ss := strings.Split(s, "\n")
+	return len(ss)
+}
+
+//─────────────┤ RemoveDupChar ├─────────────
+
+func RemoveDupChar(target string, char rune, length int) string {
+	lng := 0
+	var retstr string
+	for _, r := range target {
+		if r == char {
+			if lng < length {
+				retstr += string(r)
+				lng++
+			}
+		} else {
+			retstr += string(r)
+			lng = 0
+		}
+	}
+	return retstr
+}
